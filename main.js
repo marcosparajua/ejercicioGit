e7830be37b32f1905218a1413f6a97d597df4d11
marcos parajua <marcosparajua@gmail.com>
2024-02-22 16:37:31 +0100

    add JS

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const colors = ['black', 'brown', 'blue', 'pink'];
+  const images = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+  ];
+  let index = 0;
+
+  document.addEventListener('mousemove', () => {
+    index = (index + 1) % colors.length;
+    document.body.style.backgroundColor = colors[index];
+
+    const imgElements = document.querySelectorAll('img');
+    imgElements.forEach((img, i) => {
+      img.src = images[(index + i) % images.length];
+    });
+  });
+});
